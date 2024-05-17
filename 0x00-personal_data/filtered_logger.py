90a94eaf2bbd7b09c5cd725e1b667906e48df649
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-05-17 08:29:57 +0100

    class RedactingFormatter(logging.Formatter)

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,6 +17,28 @@ the substitution with a single regex.
 
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializer"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formatter method"""
+        text = super(RedactingFormatter, self).format(record)
+        redact = filter_datum(self.fields, self.REDACTION,
+                              text, self.SEPARATOR)
+        return redact
 
 
 def filter_datum(fields: List[str], redaction: str,
