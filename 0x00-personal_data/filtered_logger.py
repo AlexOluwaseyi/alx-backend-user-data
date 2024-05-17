127f328dc02313fe2ab36d3b7f9989c2a00df729
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-05-17 02:57:12 +0100

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Write a function called filter_datum that returns the log message obfuscated:
+
+Arguments:
+- fields: a list of strings representing all fields to obfuscate
+- redaction: a string representing by what the field will be obfuscated
+- message: a string representing the log line
+- separator: a string representing by which character is separating all
+  fields in the log line (message)
+
+The function should use a regex to replace occurrences of certain field values
+filter_datum should be less than 5 lines long and use re.sub to perform
+the substitution with a single regex.
+"""
+
+import re
+
+
+def filter_datum(fields: list, redaction: str,
+                 message: list, separator: str) -> str:
+    """Function that redacts or obfuscate PII"""
+    for field in fields:
+        redacted = re.sub(rf"(\b{re.escape(field)}=)[^;]+",
+                          rf"\1{redaction}", message)
+    return redacted
