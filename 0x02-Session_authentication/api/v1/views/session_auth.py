4260cbaaa1da0787f00a56fcd9c1cd3b226b7fb2
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-05-24 07:43:28 +0100

    Resolved issue with '/auth_session/logout' route

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -39,7 +39,7 @@ def session_login():
 
 @app_views.route('/auth_session/logout',
                  methods=['DELETE'], strict_slashes=False)
-def logout(self, request=None):
+def logout():
     """Deletes users session / logout
     """
     from api.v1.app import auth
