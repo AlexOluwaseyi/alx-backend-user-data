97a3948b1357f6d7a1902460090c210179df574a
Oluwaseyi Akintola <120264911+AlexOluwaseyi@users.noreply.github.com>
2024-05-31 09:13:23 +0100

    Update app.py
    Catch Exception for NoResultFound and InvalidRequestError in POST '/reset_password' route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,6 +6,8 @@ Basic Flask App
 
 from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
+from sqlalchemy.orm.exc import InvalidRequestError
+from sqlalchemy.exc import NoResultFound 
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -84,9 +86,12 @@ def get_reset_password_token():
             abort(403)
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token}), 200
+    except NoResultFound:
+        abort(403)
+    except InvalidRequestError:
+        abort(403)
     except ValueError:
         abort(403)
-
 
 @app.route("/reset_password", methods=['PUT'], strict_slashes=False)
 def update_password():
